23b57a67d4cdbd0303a5cfebe6f067d3b4a983ee
Risk Light <duskadik@gmail.com>
2021-11-23 23:08:59 +0200

    final edits and experiments

--- script/script.js
+++ script/script.js
@@ -1,42 +1,57 @@
 //Задание 1
-let num = 10
+let num = 10;
 console.log(num);
-let first = 5
-let second = 10
-console.log(first+second)
-console.log(first-second)
-console.log(first*second)
-console.log(first/second);
 
 //Задание 2
-let a = 3
-let b = 10
-let result = a+b
-console.log('let result' + '=' + result);
+let first = 5;
+let second = 10;
+console.log(`sum = ${first+second}, diff = ${first-second}`);
+console.log(`mult = ${first*second}, div = ${first/second}`);
 
 //Задание 3
-let bar = 20
-let qwerty = 5
-let res = bar-qwerty-10
-let end = res-qwerty;
+let a = 3;
+let b = 10;
+let result = a + b;
+console.log(`let result = ${result}`);
+
+//Задание 4
+let bar = 20;
+let qwerty = 5;
+let res = bar - qwerty - 10;
+let end = res - qwerty;
 console.log('let end' + '=' + end);
 
 //Задание 4
-let str = "Hello, Palmo"
+let str = "Hello, Palmo";
 alert(str);
 
 //Задание 5
-let name = "Олег"
-let age = "27"
-alert('Я ' + (name) + ',' + ' мне ' + (age) + ' лет');
+let name = "Олег";
+let age = "27";
+alert(`Я ${name} мне ${age} лет`);
 
 //Задание 6
-let hourSeconds = 60*60
-console.log('In hour ' + hourSeconds + ' seconds');
-let daySeconds = 60*60*24
-console.log('In day ' + daySeconds + ' seconds');
-let monthSeconds = 60*60*24*30
-console.log('In month ' + monthSeconds + ' seconds');
+const daysCurrentMonth = new Date(new Date().getFullYear(), new Date().getMonth()+1, 0).getDate();
+const hourMinutes = 60;
+const secondsMinutes = 60;
+const dayHours = 24;
+
+const hourSeconds = hourMinutes * secondsMinutes
+console.log(`In hour ${hourSeconds} seconds`);
+
+const daySeconds = hourMinutes * secondsMinutes * dayHours;
+console.log(`In day ${daySeconds} seconds`);
+
+const monthSeconds = hourMinutes * hourMinutes * dayHours * daysCurrentMonth;
+
+let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+forDate = new Date();
+let month = monthNames[forDate.getMonth()];
+console.log(`In current month ${month} ${monthSeconds} seconds`);
+
+const hourSecondsPage = document.getElementById('hour_seconds').textContent = `In hour ${hourSeconds} seconds`;
+const daySecondsPage = document.getElementById('day_seconds').textContent = `In day ${daySeconds} seconds`;
+const monthSecondsPage = document.getElementById('month_seconds').textContent = `In current month ${month} ${monthSeconds} seconds`;
 
 //Задание 7
 let q = 3
@@ -44,6 +59,6 @@ q=Math.pow(q,2);
 
 //Задание 8
 let d = 8;
-let n = Math.pow(d,2)
-let x = Math.pow(d,3)
-console.log('let n' + '=' + n +',' + ' ' + 'let x' +'=' + x);
+let n = Math.pow(d,2);
+let x = Math.pow(d,3);
+console.log(`let n = ${n}, let x = ${x}`);
