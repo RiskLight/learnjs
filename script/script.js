6d73c3b8a74fd94b95b76ca431833737d1f4767e
Risk Light <duskadik@gmail.com>
2021-11-23 11:00:37 +0200

    test final homework

--- script/script.js
+++ script/script.js
@@ -1,2 +1,55 @@
-let num = 10;
-console.log()
+//Задание 1
+let num = 10
+console.log();
+let first = 5
+let second =10
+console.log(
+    first+second
+)
+
+console.log(
+    first-second
+)
+
+console.log(
+    first*second
+)
+
+console.log(
+    first/second
+);
+
+//Задание 2
+let a = 3
+let b =10
+let result = a+b;
+
+//Задание 3
+let bar = 20
+let qwerty = 5
+let res = bar-qwerty-10
+let end = res-qwerty;
+
+//Задание 4
+let str = "Hello, Palmo"
+alert(str);
+
+//Задание 5
+let name = "Олег"
+let age = "27"
+alert('Я ' + (name) + ',' + ' мне ' + (age) + ' лет');
+
+//Задание 6
+let hour = 60*60
+console.log('In hour ' + hour + ' seconds');
+
+//Задание 7
+let q = 3
+q=Math.pow(q,2);
+
+//Задание 8
+let d = 8;
+let n
+n=Math.pow(d,2);
+let x
+x=Math.pow(d,3);
