8582947cc01f918b56de1d68ac23a1e04bcafc87
Risk Light <duskadik@gmail.com>
2021-11-23 12:55:24 +0200

    little changes

--- script/script.js
+++ script/script.js
@@ -3,32 +3,23 @@ let num = 10
 console.log();
 let first = 5
 let second =10
-console.log(
-    first+second
-)
-
-console.log(
-    first-second
-)
-
-console.log(
-    first*second
-)
-
-console.log(
-    first/second
-);
+console.log(first+second)
+console.log(first-second)
+console.log(first*second)
+console.log(first/second);
 
 //Задание 2
 let a = 3
 let b =10
-let result = a+b;
+let result = a+b
+console.log('let result' + '=' + result);
 
 //Задание 3
 let bar = 20
 let qwerty = 5
 let res = bar-qwerty-10
 let end = res-qwerty;
+console.log('let end' + '=' + end);
 
 //Задание 4
 let str = "Hello, Palmo"
@@ -40,8 +31,12 @@ let age = "27"
 alert('Я ' + (name) + ',' + ' мне ' + (age) + ' лет');
 
 //Задание 6
-let hour = 60*60
-console.log('In hour ' + hour + ' seconds');
+let hourSeconds = 60*60
+console.log('In hour ' + hourSeconds + ' seconds');
+let daySeconds = 60*60*24
+console.log('In day ' + daySeconds + ' seconds');
+let monthSeconds = 60*60*24*30
+console.log('In month ' + monthSeconds + ' seconds');
 
 //Задание 7
 let q = 3
@@ -49,7 +44,6 @@ q=Math.pow(q,2);
 
 //Задание 8
 let d = 8;
-let n
-n=Math.pow(d,2);
-let x
-x=Math.pow(d,3);
+let n = Math.pow(d,2)
+let x = Math.pow(d,3)
+console.log('let n' + '=' + n +',' + ' ' + 'let x' +'=' + x);
