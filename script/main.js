74ef86caa03712467a83a8dbfca8c02a86fa3b6e
Risk Light <duskadik@gmail.com>
2021-12-06 15:03:12 +0200

    intermediate version

--- script/main.js
+++ script/main.js
@@ -301,9 +301,6 @@ function addToCart(){
     if(!setCartData(cartData)){
         this.disabled = false; // разблокируем кнопку после обновления LS
         cartCont.innerHTML = 'Товар добавлен в корзину.';
-        setTimeout(function(){
-            cartCont.innerHTML = 'Продолжить покупки...';
-        }, 1000);
     }
     return false;
 }
@@ -342,3 +339,31 @@ addEvent(d.getElementById('clear_cart'), 'click', function(){
     localStorage.removeItem('cart');
     cartCont.innerHTML = 'Корзина очишена.';
 });
+
+
+// Получить модальный
+let modal = document.getElementById("myModal");
+
+// Получить кнопку, которая открывает модальный
+let btn = document.getElementById("myBtn");
+addEvent(d.getElementById('myBtn'), 'click', openCart);
+
+// Получить элемент <span>, который закрывает модальный
+let span = document.getElementsByClassName("close")[0];
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Когда пользователь щелкает в любом месте за пределами модального, закройте его
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
